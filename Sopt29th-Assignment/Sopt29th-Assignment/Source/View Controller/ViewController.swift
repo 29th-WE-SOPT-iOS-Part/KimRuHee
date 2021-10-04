e956d25b7dee9e10c92030b056f8f3ffeb2e6d9b
heerucan <iwpow0307@gmail.com>
2021-10-05 02:34:32 +0900

    ➕[ADD] 과제 프로젝트 파일 생성(#2)

---
+++ Sopt29th-Assignment/Sopt29th-Assignment/Source/View Controller/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Sopt29th-Assignment
+//
+//  Created by Thisisme Hi on 2021/10/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configUI()
+        setupAutoLayout()
+    }
+    
+    // MARK: - Custom Method
+    func configUI() {
+        view.backgroundColor = .white
+    }
+    
+    func setupAutoLayout() {
+        
+    }
+}
+
