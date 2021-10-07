5ea21722d9be54b02ae00d34e1e841964064ea76
heerucan <iwpow0307@gmail.com>
2021-10-08 02:57:14 +0900

    ➕[ADD] GoogleService-Info.plist 추가(#4)

--- Sopt29th-Assignment/Sopt29th-Assignment/Resource/Support/AppDelegate.swift
+++ Sopt29th-Assignment/Sopt29th-Assignment/Resource/Support/AppDelegate.swift
@@ -7,13 +7,15 @@
 
 import UIKit
 
+import Firebase
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
