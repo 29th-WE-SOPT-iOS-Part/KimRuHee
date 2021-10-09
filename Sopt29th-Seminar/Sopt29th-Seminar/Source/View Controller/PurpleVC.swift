7ad02e95abc58a35794b8e79f1d6897e3790c57c
heerucan <iwpow0307@gmail.com>
2021-10-09 22:26:01 +0900

    ➕[ADD] 2nd seminar update

---
+++ Sopt29th-Seminar/Sopt29th-Seminar/Source/View Controller/PurpleVC.swift
@@ -0,0 +1,29 @@
+//
+//  PurpleVC.swift
+//  Sopt29th-Seminar
+//
+//  Created by Thisisme Hi on 2021/10/09.
+//
+
+import UIKit
+
+class PurpleVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
