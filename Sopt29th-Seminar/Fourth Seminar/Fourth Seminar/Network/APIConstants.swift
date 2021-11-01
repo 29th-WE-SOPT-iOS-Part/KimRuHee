e6d4d1707d264c4678bb157d18b19549edccb8d7
heerucan <iwpow0307@gmail.com>
2021-11-02 02:07:24 +0900

    ➕[ADD] 4th seminar update

---
+++ Sopt29th-Seminar/Fourth Seminar/Fourth Seminar/Network/APIConstants.swift
@@ -0,0 +1,16 @@
+//
+//  APIConstants.swift
+//  Fourth Seminar
+//
+//  Created by Thisisme Hi on 2021/10/30.
+//
+
+import Foundation
+
+struct APIConstants {
+    
+    static let baseURL = "https://asia-northeast3-we-sopt-29.cloudfunctions.net/api"
+    
+    static let loginURL = baseURL + "/user/login"
+    static let readUserURL = baseURL + "/user/"
+}
