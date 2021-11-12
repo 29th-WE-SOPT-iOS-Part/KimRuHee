2dd884a13b9abe438890e299da90f6e9bfac0df5
heerucan <iwpow0307@gmail.com>
2021-11-12 16:30:55 +0900

    ✨[FEAT] 시작점 변경 및 프로필 버튼 선택 시 로그인 전환(#12)

--- Sopt29th-Assignment/Sopt29th-Assignment/Resource/Support/SceneDelegate.swift
+++ Sopt29th-Assignment/Sopt29th-Assignment/Resource/Support/SceneDelegate.swift
@@ -15,7 +15,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        let navigationController = UINavigationController(rootViewController: HomeVC())
+        let navigationController = UINavigationController(rootViewController: MainTabVC())
+        navigationController.navigationBar.isHidden = true
         window?.rootViewController = navigationController
         window?.backgroundColor = .white
         window?.makeKeyAndVisible()
